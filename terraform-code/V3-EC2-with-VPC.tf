8b413602ef3de22e642a1cea24a5eb0b494281c3
jchauhan42@outlook.com <jchauhan42@outlook.com>
2024-07-24 12:03:56 +0530

    commiting terraform-code/V3-EC2-with-VPC.tf

--- terraform-code/V3-EC2-with-VPC.tf
+++ terraform-code/V3-EC2-with-VPC.tf
@@ -9,10 +9,6 @@ resource "aws_instance" "demo-server" {
   #security_groups         = [ "demo-sg" ]
   vpc_security_group_ids = [ aws_security_group.demo-sg.id ]
   subnet_id = aws_subnet.dpp-public-subnet-01.id
-  for_each = toset(["build-slave","ansible"])
-  tags = {
-    Name = "${each.key}"
-  }
 }
 
 resource "aws_security_group" "demo-sg" {
